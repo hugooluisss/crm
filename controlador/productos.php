8d9085df1402ff1e760529c89ee1e2b882ed7da5
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-05-10 10:30:33 -0500

    Productos en la app

--- controlador/productos.php
+++ controlador/productos.php
@@ -63,9 +63,43 @@ switch($objModulo->getId()){
 			case 'validaCodigo':
 				global $userSesion;
 				$db = TBase::conectaDB();
-				$rs = $db->Execute("select idProducto from producto a join categoria b using(idCategoria) where clave = '".$_POST['txtClave']."' and idEmpresa = ".$userSesion->empresa->getId()." and not idProducto = '".$_POST['id']."'");
+				$rs = $db->Execute("select idProducto from producto a join categoria b using(idCategoria) where clave = '".$_POST['txtClave']."' and idEmpresa = ".($_POST['empresa'] == ''?$userSesion->empresa->getId():$_POST['empresa'])." and not idProducto = '".$_POST['id']."'");
 				
 				echo $rs->EOF?"true":"false";
+			break;
+			
+			#App
+			case 'getPanel':
+				$db = TBase::conectaDB();
+				global $userSesion;
+				$rs = $db->Execute("select * from categoria where idEmpresa = ".$_POST['empresa']);
+				$datos = array();
+				while (!$rs->EOF){
+					array_push($datos, $rs->fields);
+					$rs->moveNext();
+				}
+				
+				$rs = $db->Execute("select * from tipoproducto");
+				$datos2 = array();
+				while (!$rs->EOF){
+					array_push($datos2, $rs->fields);
+					$rs->moveNext();
+				}
+				
+				echo json_encode(array("categorias" => $datos, "tipos" => $datos2));
+			break;
+			case 'lista':
+				$db = TBase::conectaDB();
+
+				$rs = $db->Execute("select a.*, b.nombre as categoria, c.nombre as tipo from producto a join categoria b using(idCategoria) join tipoproducto c using(idTipo) where idEmpresa = ".$_POST['empresa']);
+				$datos = array();
+				while(!$rs->EOF){
+					array_push($datos, $rs->fields);
+					
+					$rs->moveNext();
+				}
+		
+				echo json_encode($datos);
 			break;
 		}
 	break;
